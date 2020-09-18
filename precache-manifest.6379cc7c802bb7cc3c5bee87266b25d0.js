8f10a57beb745fd9ad595c5e33320da6b2363ae5
Hassan Elseoudy <hassan.elseoudy@gmail.com>
2020-09-18 17:19:14 +0200

    Updates

---
+++ precache-manifest.6379cc7c802bb7cc3c5bee87266b25d0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf999020090a77ae826d7bb1b0c30ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d4a78edcf28eadafae",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f19d609d6979c13b2286",
+    "url": "/static/css/main.86ce556b.chunk.css"
+  },
+  {
+    "revision": "d7d4a78edcf28eadafae",
+    "url": "/static/js/2.a1881035.chunk.js"
+  },
+  {
+    "revision": "0001512ff5376eafd7f76369861b7a6e",
+    "url": "/static/js/2.a1881035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f19d609d6979c13b2286",
+    "url": "/static/js/main.1fadb170.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "d71c16d1c7d29ef4416933dbb92fb16d",
+    "url": "/static/media/Hassan-El-seoudy-Resume2020.d71c16d1.pdf"
+  },
+  {
+    "revision": "56c3100b83c5c00f31c7ce88de9c514c",
+    "url": "/static/media/Profile.56c3100b.png"
+  },
+  {
+    "revision": "5e05942ee0c41ee3775312b63afc245b",
+    "url": "/static/media/Robotech.5e05942e.png"
+  },
+  {
+    "revision": "ed00387d969fe3c9a17f5d53196b3a01",
+    "url": "/static/media/sia.ed00387d.png"
+  }
+]);
