4d90f6e34a1711dd38ae6c2b549d4372a4bc01a4
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2022-09-25 02:24:15 +0200

    Updates

---
+++ precache-manifest.921ce9bc632f8c9a84250e183fb87990.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a79bc60171a70848a024067d188a10f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edad61c9ef38b77de93b",
+    "url": "/static/css/main.fc2e3a11.chunk.css"
+  },
+  {
+    "revision": "1bbdc89343911873c681",
+    "url": "/static/js/2.f465e253.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.f465e253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edad61c9ef38b77de93b",
+    "url": "/static/js/main.42fa6824.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "a73eee604c06aa36e7ba1972c8e9200c",
+    "url": "/static/media/Zalando-SE-logo.a73eee60.png"
+  },
+  {
+    "revision": "27a7c712a28d9be21af680c1d35aa48f",
+    "url": "/static/media/cynopsis.27a7c712.png"
+  },
+  {
+    "revision": "e0935e18e08dd2620a8c9a7f4eb9d25e",
+    "url": "/static/media/dhl.e0935e18.jpg"
+  },
+  {
+    "revision": "4f1080ea53d62bd0a2fe0c7e27720566",
+    "url": "/static/media/hassanelseoudy.4f1080ea.png"
+  },
+  {
+    "revision": "f60319d8ce6a7363ed97071907927e44",
+    "url": "/static/media/resume.f60319d8.pdf"
+  },
+  {
+    "revision": "7d42b7ae94a2dfc4614aeed779ea892c",
+    "url": "/static/media/robotech.7d42b7ae.png"
+  },
+  {
+    "revision": "8051c654383e761fb71e4f214c38d83f",
+    "url": "/static/media/rubikal.8051c654.png"
+  },
+  {
+    "revision": "fe93b0cceeea6ad272a9ae0bd5a81646",
+    "url": "/static/media/sia.fe93b0cc.jfif"
+  }
+]);
